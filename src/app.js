903590461f628acdc5708fe2a95edf4340b4e908
JJ Crossman <jj.crossman@gmail.com>
2016-11-14 23:28:02 -0600

    angular is up and running

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
-angular.module( "CrossmanPortfolio", [ "ui-router" ] )
 
-.config( )
+
+angular.module( "CrossmanPortfolio", [ "ui.router" ] )
+
+.config( function ( $stateProvider, $urlRouterProvider ) {
+
+  $urlRouterProvider.otherwise( "/" );
+
+  $stateProvider
+    .state( "home", {
+      url: "/"
+      , templateUrl: "./src/home-tmpl.html"
+      , controller: "homeCtrl"
+    } );
+    // .state( "about", {
+    //   url: "/about"
+    //   , template: aboutTmpl
+    //   , controller: "aboutCtrl"
+    // } );
+
+} );
